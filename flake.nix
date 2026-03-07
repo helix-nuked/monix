e047eaac37336dac8a35c6c5e6ea964a95424bfc
monyx <helix.nuked@proton.me>
2026-03-07 12:05:29 +0500

    maybe that will work

--- flake.nix
+++ flake.nix
@@ -139,12 +139,12 @@
     cores = 0;
     # auto-optimise-store = true;
     extra-substituters = [
-      # "https://install.determinate.systems"
-      # "https://nix-community.cachix.org"
+      "https://install.determinate.systems"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      # "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     extra-trusted-users = [
       "monyx"
